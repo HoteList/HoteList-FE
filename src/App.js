f81d23de6279817570edb7d24c21ac2ace656fa9
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-03-23 23:18:38 +0700

    feat: protected route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import Home from "./pages/Home";
-import { persistor, store } from "./redux/store";
+import Routers from './routes';
 
 function App() {
   return (
-    <div>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Router>
-            <Routes>
-              <Route>
-                <Route path="/" element={<Home />} />
-              </Route>
-            </Routes>
-          </Router>
-        </PersistGate>
-      </Provider>
-    </div>
+    <Routers />
   );
 }
 
